95d082c7501d9e29228d2fa45cbe5d64c368c53e
imonweb <contact@imonweb.co.uk>
2021-06-05 09:25:57 +0100

    header section

---
+++ app.js
@@ -0,0 +1,16 @@
+const menuBtn = document.getElementById('menuBtn');
+const sideNav = document.getElementById('sideNav');
+const menu = document.getElementById('menu');
+
+sideNav.style.right = "-180px";
+
+menuBtn.addEventListener('click', () => {
+   
+  if(sideNav.style.right == "-180px") {
+    sideNav.style.right = "0";
+    menu.src = "images/close.png";
+  } else {
+    sideNav.style.right = "-180px";
+    menu.src = "images/menu.png";
+  }
+})
